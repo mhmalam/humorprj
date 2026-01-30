473590784ede48e26bcffd219affd6666d3311a3
Yin <isbestsalt@gmail.com>
2026-01-30 14:34:27 -0500

    Week 1 Hello World

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default HomePage;
